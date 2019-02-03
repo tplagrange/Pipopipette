778ff124d0a66fcfd6a4fabaf6c2471e32a26199
Thomas Lagrange <tplagrange@gmail.com>
2019-02-02 21:10:03 -0500

    Initial Commit

---
+++ Pipopipette/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Pipopipette
+//
+//  Created by Thomas Lagrange on 2/2/19.
+//  Copyright © 2019 Thomas Lagrange. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
